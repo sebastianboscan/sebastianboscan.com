dddb08b514c6759b06bac20f57e0db93e84337f9
sebastianboscan <sebastianboscan@hotmail.fr>
2025-12-20 21:21:33 -0500

    First commit

---
+++ sebastianboscan/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center font-sans">
+      <div className="text-center">
+        <h1>Hello!</h1>
+        <p>This is Sebastian</p>
+      </div>
+    </div>
+  );
+}
