15b8aaba87b7db93e2a79ef82c72c2905a5fc60f
Sebastian Boscan-Villalobos <64043490+sebastianboscan@users.noreply.github.com>
2025-12-24 23:05:33 -0500

    Feat/header - Add very simple header to website (#3)
    * Add prototype header to website
    
    * Delete .vscode directory

--- sebastianboscan/app/page.tsx
+++ sebastianboscan/app/page.tsx
@@ -2,11 +2,18 @@ import Image from "next/image";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-black text-white flex items-center justify-center font-sans">
-      <div className="text-center">
-        <h1>Hello!</h1>
-        <p>This is Sebastian</p>
-        {/* <Image src="/images/headshot.jpg" width="200" height="200" alt="Headshot of Sebastian"/> */}
+    <div className="min-h-screen bg-black text-white font-sans">
+      <header className="p-6">
+        <nav className="max-w-7xl mx-auto">
+          <h2>Sebastian</h2>
+        </nav>
+      </header>
+      
+      <div className="flex items-center justify-center" style={{ minHeight: 'calc(100vh - 80px)' }}>
+        <div className="text-center">
+          <h1>Hello!</h1>
+          <p>This is Sebastian</p>
+        </div>
       </div>
     </div>
   );
